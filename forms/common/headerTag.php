65564b0a794ed9646b7e942e6b6a6b1d01f4a111
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-10-28 09:48:31 +0800

    add forget form

--- forms/common/headerTag.php
+++ forms/common/headerTag.php
@@ -5,7 +5,7 @@
 	<meta name="author" content="Akram Tello">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<meta name="description" content="This is a login page">
-	<title>Login Page</title>
+	<title>PHP form</title>
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 </head>
 <body>
--- forms/forget.php
+++ forms/forget.php
@@ -1,6 +1,6 @@
 <?php
 // include headerTag.php file
-include ('forms/common/headerTag.php');
+include_once __DIR__ . '/common/headerTag.php';
 ?>
 
 <section class="h-100">
@@ -8,11 +8,11 @@ include ('forms/common/headerTag.php');
         <div class="row justify-content-sm-center h-100">
             <div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
                 <div class="text-center my-5">
-                    <img src="../assets/img/login.png" alt="login" width="100">
+                    <img src="../assets/img/forget.png" alt="forget" width="100">
                 </div>
                 <div class="card shadow-lg">
                     <div class="card-body p-5">
-                        <h1 class="fs-4 card-title fw-bold mb-4">Login</h1>
+                        <h1 class="fs-4 card-title fw-bold mb-4">Forgot Password</h1>
                         <form method="POST" class="needs-validation" novalidate="" autocomplete="off">
                             <div class="mb-3">
                                 <label class="mb-2 text-muted" for="email">E-Mail Address</label>
@@ -22,33 +22,16 @@ include ('forms/common/headerTag.php');
                                 </div>
                             </div>
 
-                            <div class="mb-3">
-                                <div class="mb-2 w-100">
-                                    <label class="text-muted" for="password">Password</label>
-                                    <a href="./forms/forget.php" class="float-end">
-                                        Forgot Password?
-                                    </a>
-                                </div>
-                                <input id="password" type="password" class="form-control" name="password" required>
-                                <div class="invalid-feedback">
-                                    Password is required
-                                </div>
-                            </div>
-
                             <div class="d-flex align-items-center">
-                                <div class="form-check">
-                                    <input type="checkbox" name="remember" id="remember" class="form-check-input">
-                                    <label for="remember" class="form-check-label">Remember Me</label>
-                                </div>
                                 <button type="submit" class="btn btn-primary ms-auto">
-                                    Login
+                                    Send Link	
                                 </button>
                             </div>
                         </form>
                     </div>
                     <div class="card-footer py-3 border-0">
                         <div class="text-center">
-                            Don't have an account? <a href="./forms/register.php" class="text-dark">Create One</a>
+                            Remember your password? <a href="../index.php" class="text-dark">Login</a>
                         </div>
                     </div>
                 </div>
@@ -59,5 +42,5 @@ include ('forms/common/headerTag.php');
 
 <?php
 // include scriptTag.php file
-include ('forms/common/scriptTag.php');
+include_once __DIR__ . '/common/scriptTag.php';
 ?>
