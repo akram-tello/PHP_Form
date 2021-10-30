8ff9f34d3a8fbb63a9adb8e8af6dbcd0931e3fe2
Akramtello <35160721+akram-tello@users.noreply.github.com>
2021-10-30 09:49:32 +0800

    include header

--- forms/register.php
+++ forms/register.php
@@ -1,21 +1,10 @@
 <?php
 // include backend server
 include ('..\backend\server.php');
+
+// include headerTag.php file
+include ('forms/common/headerTag.php');
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="utf-8">
-	<meta name="author" content="Akram Tello">
-	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<meta name="description" content="This is a login page">
-	<title>PHP form</title>
-	<!-- Favicon-->
-	<link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-	<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-</head>
-<body>
 
 <section class="h-100">
 	<div class="container h-100">
